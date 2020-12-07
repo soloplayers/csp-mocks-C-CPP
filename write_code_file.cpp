c70cd0f3172c52133eba5cb446c3faaf81ffce2b
soloplayers <937059391@qq.com>
2020-12-07 19:19:37 +0800

    csp problem 201912-1

--- write_code_file.cpp
+++ write_code_file.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
 using namespace std;
-
-struct P
+bool isPass(int a)
 {
-    int index,value;
-};
-
+    if(a%7==0)
+    {
+        return true;
+    }
+    while (a)
+    {
+        if(a%10==7)
+        {
+            return true;
+        }
+        a/=10;
+    }
+    return false;
+}
 int main() {
-    int n, a, b;
-    cin >> n >> a >> b;
-    struct P U[a + 1];
-    for (int i = 0; i < a; i++) {
-        cin >> U[i].index >> U[i].value;
-    }
-    long long result = 0; //取值的绝对值为10^6，乘积为10^12，必须用long long型
-    int pointer = 0; //U向量的记录型指针
-    int index, value;
-    for (int i = 0; pointer < a && i < b; i++) {
-        cin >> index >> value;
-        while (pointer < a && index > U[pointer].index) pointer++;
-        if (index > U[a - 1].index) break; //如果V向量输入的index大于了U向量最大的index，后面的全是0不用考虑
-        if (pointer >= a) break;    //U向量对应的数据已经计算完，后面V向量的输入不用再考虑
-        if (index == U[pointer].index) result += U[pointer++].value * value;//计算完后pointer指针增1
-    }
-    cout << result;
+   int n,count=1;
+   cin>>n;
+   int people[4]={0};
+   for(int i=0;i<n;i++)
+   {
+       if(isPass(count))
+       {
+           n++;
+           people[i%4]+=1;
+       }
+       count++;
+   }
+   /*
+    * 这个语句过不了csp的编译真是服了
+    *
+    * for(int i:people)
+   {
+       cout<<i<<endl;
+   }*/
+   for(int i=0;i<4;i++)
+   {
+       cout<<people[i]<<endl;
+   }
 }
