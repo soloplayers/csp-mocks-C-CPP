67421837c446bc225d4231479ff1f468e50289e4
soloplayers <937059391@qq.com>
2020-12-11 13:26:25 +0800

    csp problem 201903-1

--- write_code_file.cpp
+++ write_code_file.cpp
@@ -1,35 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-    int N, M, ai;
-    int positive;//统计最新的正整数
-    int flag_minus;//统计最新的蔬果数
-    bool flag[1001] = {false};
-    int T = 0, D = 0, E = 0;
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> M >> positive;
-        flag_minus = 0;
-        for (int j = 1; j < M; j++) {
-            cin >> ai;
-            if (ai > 0) {
-                if (!flag[i] && positive - flag_minus != ai) {
-                    flag[i] = true;
-                    D++;
-                }
-                positive = ai;
-                flag_minus = 0;
-            } else {
-                flag_minus -= ai;
-            }
-        }
-        T += positive - flag_minus;
+    int n;
+    cin>>n;
+    int max,min;
+    int a[n+1];
+    for (int i=0;i<n;i++){
+        cin>>a[i];
     }
-    /* 围成一个圆圈，一般使用%取余操作 */
-    for (int i = 0; i < N; i++) {
-        if (flag[i] && flag[(i + 1) % N] && flag[(i + 2) % N]) {
-            E++;
+    max=a[0]>a[n-1]? a[0]:a[n-1];
+    min=a[0]>a[n-1]? a[n-1]:a[0];
+    cout<<max<<" ";
+    if(n%2){
+        cout<<a[n/2]<<" ";
+    }
+    else {
+        if(a[n/2-1]%2==a[n/2]%2){
+            cout<<(a[n/2-1]+a[n/2])/2<<" ";
+        }
+        else {
+            cout<<(double)(a[n/2-1]+a[n/2])/2<<" ";
         }
     }
-    cout << T << " " << D << " " << E << endl;
+    cout<<min;
+    return 0;
 }
