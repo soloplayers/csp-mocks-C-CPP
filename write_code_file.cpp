6bfc72dd29c4e82ae2c19c24366160efbae7ab9a
soloplayers <937059391@qq.com>
2020-12-12 17:48:43 +0800

    csp problem 201903-2

--- write_code_file.cpp
+++ write_code_file.cpp
@@ -1,27 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
+stack<int> num;
+stack<char> op;
+void calculate(char x) {
+    int n1 = num.top();
+    num.pop();
+    int n2 = num.top();
+    num.pop();
+    switch (x) {
+        case '+':
+            num.push(n1 + n2);
+            break;
+        case 'x':
+            num.push(n1 * n2);
+            break;
+        case '/':
+            num.push(n2 / n1);
+            break;
+    }
+}
 int main() {
     int n;
-    cin>>n;
-    int max,min;
-    int a[n+1];
-    for (int i=0;i<n;i++){
-        cin>>a[i];
-    }
-    max=a[0]>a[n-1]? a[0]:a[n-1];
-    min=a[0]>a[n-1]? a[n-1]:a[0];
-    cout<<max<<" ";
-    if(n%2){
-        cout<<a[n/2]<<" ";
-    }
-    else {
-        if(a[n/2-1]%2==a[n/2]%2){
-            cout<<(a[n/2-1]+a[n/2])/2<<" ";
+    string s;
+    cin >> n;
+    while (n--) {
+        while (!num.empty()) num.pop();
+        while (!op.empty()) op.pop();
+        cin >> s;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] >= '1' && s[i] <= '9')
+                num.push(s[i] - '0');
+            else if (s[i] == 'x' || s[i] == '/') {
+                num.push(s[i + 1] - '0');
+                calculate(s[i]);
+                i++;
+            } else if (s[i] == '-') {
+                op.push('+');
+                num.push(-(s[i + 1] - '0'));
+                i++;
+            } else op.push(s[i]);
         }
-        else {
-            cout<<(double)(a[n/2-1]+a[n/2])/2<<" ";
+        while (!op.empty()) {
+            calculate(op.top());
+            op.pop();
         }
+        num.top() == 24 ? cout << "Yes" << endl : cout << "No" << endl;
     }
-    cout<<min;
     return 0;
 }
